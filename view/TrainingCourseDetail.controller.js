b619c0af64dff967c1314486189ceff2e2177b6d
sergey.nikitin <sergey.nikitin@saprun.com>
2014-11-07 01:53:32 +0400

    add modules and mockups

--- view/TrainingCourseDetail.controller.js
+++
@@ -1,6 +0,0 @@
-sap.ui.controller("view.TrainingCourseDetail", {
-	onBeforeRendering:function(){
-		console.log(this.getView().getModel())
-		//this.byId("SupplierForm").bindElement("BusinessPartner");
-	}
-});
